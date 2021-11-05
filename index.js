511fc26d60e18d89399f49fa8ec336b3fbc10370
LeoZhang2016 <zhanglong7511@gmail.com>
2021-11-05 20:44:15 +0800

    assignment completed

---
+++ index.js
@@ -0,0 +1,10 @@
+
+ window.onload = function () {
+     document.querySelector(".menu-btn").addEventListener('click',()=>{
+         document.querySelector('.main-menu').classList.toggle('show');
+     });
+
+     // document.querySelector(".menu-btn").addEventListener('click',()=>{
+     //     alert("111");
+     // })
+ }
